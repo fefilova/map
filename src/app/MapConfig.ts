b7333dda8bc9fd72911f5af4d742756878bbbf11
unknown <allblue312@gmail.com>
2021-03-20 12:27:26 +0300

    first commit

---
+++ src/app/MapConfig.ts
@@ -0,0 +1,8 @@
+export default interface MapConfig{
+    sities: {
+        armalit: string
+        askold: string
+        burevestnik: string
+        skbk: string
+    }
+}
---
+++ src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+import MapConfig from './MapConfig';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  armalit_img
+  askold_img
+  burevestnik_img
+  skbk_img
+  constructor(private httpClient: HttpClient) {
+    this.get.subscribe((x: MapConfig) => {
+        this.armalit_img = x.sities.armalit;
+        this.askold_img = x.sities.askold;
+        this.burevestnik_img = x.sities.burevestnik;
+        this.skbk_img = x.sities.skbk;
+        console.log(this.armalit_img);
+      })
+  }
+  
+  
+  get = this.httpClient.get<MapConfig>('sities.json')
+}
